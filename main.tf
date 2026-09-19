ebb1b1b7d8434e7bab73371c79cde1a04c0a5a8a
Hussein Alattas <hussein@Husseins-MacBook-Air.local>
2026-09-19 17:36:24 +0200

    Pushing main.tf to GH

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-user18"
-    storage_account_name = "gastuser182026"
+    resource_group_name  = "RG-00"
+    storage_account_name = "storageaccountftfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
