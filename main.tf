63b3a7f288558a5611ffa38f3d46d2ba16a4bd49
Hussein Alattas <hussein@Husseins-MacBook-Air.local>
2026-09-20 19:41:24 +0200

    correcting syntax

--- main.tf
+++ main.tf
@@ -22,8 +22,10 @@ terraform {
 module "keyvault" {
   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
   keyvault_name = "gakvuser1820"
-  resource_group = var.resource_group
-  location = var.location
+  resource_group = {
+    name = var.resource_group
+    location = var.location
+  }
   network_acls = {
     bypass = "AzureServices"
   }
@@ -34,8 +36,11 @@ module "keyvault" {
 module "mssql_server" {
   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=mssql_server/v1.0.0"
   # also any inputs for the module (see below)
-  resource_group = var.resource_group
-  location = var.location
+  resource_group = {
+    name = var.resource_group
+    location = var.location
+  }
+
   sql_server_admin = "sql-admin"
   sql_server_name = "sql-server-name"
   sql_server_version = "12"
